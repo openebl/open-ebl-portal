70c8b2499c812858c976a075ae9b6b7b61801832
Kai-Wen Jong <kevin@bluextrade.com>
2024-02-25 23:01:25 +0800

    add react-springs to cross fade confirmation dialog

--- src/app/_components/dialogs/confirmation-dialog.tsx
+++ src/app/_components/dialogs/confirmation-dialog.tsx
@@ -1,3 +1,5 @@
+import { animated, useTransition } from "@react-spring/web";
+
 import {
   AlertDialog,
   AlertDialogAction,
@@ -20,6 +22,66 @@ export type DialogContent = {
   };
 };
 
+const ConfirmContent = ({
+  content,
+  onCancel,
+  onConfirm,
+}: {
+  content: DialogContent;
+  onCancel?: () => void;
+  onConfirm: () => void;
+}) => {
+  return (
+    <>
+      <AlertDialogHeader className="min-h-[164px] min-w-[500px]">
+        <AlertDialogTitle>{content.confirm?.title}</AlertDialogTitle>
+        <AlertDialogDescription>
+          {content.confirm?.message}
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel onClick={onCancel}>
+          {content.confirm?.cancelButton ?? "Cancel"}
+        </AlertDialogCancel>
+        <AlertDialogAction onClick={onConfirm}>
+          {content.confirm?.confirmButton ?? "OK"}
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </>
+  );
+};
+
+const WaitingContent = ({ content }: { content: DialogContent }) => {
+  return (
+    <AlertDialogHeader className="min-h-[220px] min-w-[500px] items-center justify-center space-y-5 p-[1.875rem]">
+      {content.waiting?.icon}
+      <AlertDialogTitle>{content.waiting?.message}</AlertDialogTitle>
+    </AlertDialogHeader>
+  );
+};
+
+const CompletedContent = ({
+  content,
+  onConfirm,
+}: {
+  content: DialogContent;
+  onConfirm: () => void;
+}) => {
+  return (
+    <>
+      <AlertDialogHeader className="min-h-[164px] min-w-[500px] items-center justify-center space-y-5 p-[1.875rem]">
+        {content.completed?.icon}
+        <AlertDialogTitle>{content.completed?.message}</AlertDialogTitle>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogAction onClick={onConfirm}>
+          {content.completed?.confirmButton ?? "OK"}
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </>
+  );
+};
+
 export const ConfirmationDialog = ({
   open,
   state,
@@ -33,48 +95,33 @@ export const ConfirmationDialog = ({
   onCancel?: () => void;
   onConfirm: () => void;
 }) => {
+  const transitions = useTransition(state, {
+    from: { position: "absolute", opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
   return (
     <AlertDialog open={open}>
-      <AlertDialogContent className="min-w-[500px]">
-        {state === "confirm" && (
-          <>
-            <AlertDialogHeader className="min-h-[164px]">
-              <AlertDialogTitle>{content.confirm?.title}</AlertDialogTitle>
-              <AlertDialogDescription>
-                {content.confirm?.message}
-              </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogCancel onClick={onCancel}>
-                {content.confirm?.cancelButton ?? "Cancel"}
-              </AlertDialogCancel>
-              <AlertDialogAction onClick={onConfirm}>
-                {content.confirm?.confirmButton ?? "OK"}
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </>
-        )}
-
-        {state === "waiting" && (
-          <AlertDialogHeader className="min-h-[220px] items-center justify-center space-y-5 p-[1.875rem]">
-            {content.waiting?.icon}
-            <AlertDialogTitle>{content.waiting?.message}</AlertDialogTitle>
-          </AlertDialogHeader>
-        )}
-
-        {state === "completed" && (
-          <>
-            <AlertDialogHeader className="min-h-[164px] items-center justify-center space-y-5 p-[1.875rem]">
-              {content.completed?.icon}
-              <AlertDialogTitle>{content.completed?.message}</AlertDialogTitle>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-              <AlertDialogAction onClick={onConfirm}>
-                {content.completed?.confirmButton ?? "OK"}
-              </AlertDialogAction>
-            </AlertDialogFooter>
-          </>
-        )}
+      <AlertDialogContent className="min-h-[220px] min-w-[500px]">
+        {transitions((style, item) => (
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-expect-error
+          <animated.div style={{ ...style }}>
+            {item === "confirm" ? (
+              <ConfirmContent
+                content={content}
+                onCancel={onCancel}
+                // onConfirm={onConfirm}
+                onConfirm={onConfirm}
+              />
+            ) : item === "waiting" ? (
+              <WaitingContent content={content} />
+            ) : (
+              <CompletedContent content={content} onConfirm={onConfirm} />
+            )}
+          </animated.div>
+        ))}
       </AlertDialogContent>
     </AlertDialog>
   );
